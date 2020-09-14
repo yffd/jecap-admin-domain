64ed201b1e74f3177958dcd6a20d98386673bfe1
zhangshitao <zhangshitao@thinkinpets.com>
2020-09-14 10:30:50 +0800

    init

---
+++ src/main/java/com/yffd/jecap/admin/domain/sys/user/repo/ISysUserRepo.java
@@ -0,0 +1,7 @@
+package com.yffd.jecap.admin.domain.sys.user.repo;
+
+import com.yffd.jecap.admin.domain.sys.user.entity.SysUser;
+import com.yffd.jecap.common.base.repository.IBaseRepository;
+
+public interface ISysUserRepo extends IBaseRepository<SysUser> {
+}
---
+++ src/main/java/com/yffd/jecap/admin/domain/sys/user/service/SysUserService.java
@@ -0,0 +1,18 @@
+package com.yffd.jecap.admin.domain.sys.user.service;
+
+import com.yffd.jecap.admin.domain.sys.user.entity.SysUser;
+import com.yffd.jecap.admin.domain.sys.user.repo.ISysUserRepo;
+import com.yffd.jecap.common.base.repository.IBaseRepository;
+import com.yffd.jecap.common.base.service.AbstractBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SysUserService extends AbstractBaseService<SysUser> {
+    @Autowired private ISysUserRepo sysUserRepo;
+
+    @Override
+    protected IBaseRepository getRepo() {
+        return sysUserRepo;
+    }
+}
